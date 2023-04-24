19372aeeed57402db45f49b8726c47f98807ef63
adaxiik <a.jantula@seznam.cz>
2023-04-24 22:13:53 +0200

    Login page

---
+++ AdminUI/Login.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace AdminUI;
+using DataLayer.Models;
+using DataLayer;
+using System.Collections.Generic;
+
+
+public partial class Login : Window
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+}
